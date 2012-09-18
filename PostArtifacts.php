50bd5b79ff8764dd68f15019f7c157ec8785064f
mastram <mastram@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-18 09:14:38 +0000

    git-svn-id: https://plugins.svn.wordpress.org/hanu-droid/trunk@600398 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ PostArtifacts.php
@@ -0,0 +1,95 @@
+<?php
+/*  Copyright 2012  Varun Verma  (email : varunverma@varunverma.org)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+header("Content-type: text/xml");
+
+require( dirname(dirname(dirname(dirname(__FILE__)))) . '/wp-config.php' );
+
+global $wpdb;
+
+$host = DB_HOST;
+$user = DB_USER;
+$pass = DB_PASSWORD;
+$database = DB_NAME;
+
+$linkID = mysql_connect($host, $user, $pass) or die("Could not connect to host.");
+mysql_select_db($database, $linkID) or die("Could not find database.");
+
+$mod_time = $_POST[modified_time];
+
+if($mod_time ==  ""){
+    die("No input!");
+}
+
+$post_table_name = $wpdb->prefix.'posts';
+$post_meta_table_name = $wpdb->prefix.'postmeta';
+$comments_table_name = $wpdb->prefix.'comments';
+
+$query = "SELECT ID, post_date, post_modified FROM $post_table_name WHERE post_modified >= '$mod_time' AND post_status = 'publish' ".
+			"ORDER BY post_date DESC LIMIT 50";
+$result = mysql_query($query, $linkID) or die("Records not found.");
+
+$xml_output = "<?xml version=\"1.0\"?>\n";
+
+$xml_output .= "<PostArtificats>\n";
+
+for($x = 0 ; $x < mysql_num_rows($result) ; $x++){
+
+	$row = mysql_fetch_assoc($result);
+    $id = $row['ID'];
+
+    $xml_output .= "<PostArtifcatData \n";
+    $xml_output .= 'Id="'.$id.'"'."\n";
+	$xml_output .= 'PublishDate="'.$row['post_date'].'"'."\n";
+	$xml_output .= 'ModifiedDate="'.$row['post_modified'].'"'."\n";
+
+	// Post Meta Data
+	$post_meta_query = "SELECT meta_value FROM $post_meta_table_name WHERE post_id = '$id' AND meta_key = 'ratings_average'";
+	$post_meta_result = mysql_query($post_meta_query, $linkID);
+	$xml_output .= "AverageRating=\"";
+	$rating = "0";
+	if($post_meta_result){
+		// If we have something
+		for($y = 0 ; $y < mysql_num_rows($post_meta_result) ; $y++){
+			$post_meta_row = mysql_fetch_assoc($post_meta_result);
+			$rating = $post_meta_row['meta_value'];
+		}
+	}
+	
+	$xml_output .= $rating.'"'."\n";
+
+	// Post Comments Data.
+	$comments_query = "SELECT comment_date FROM $comments_table_name WHERE comment_post_ID = '$id' and comment_approved = '1'
+						ORDER BY comment_date_gmt desc LIMIT 1";
+	$comments_result = mysql_query($comments_query, $linkID);	
+	$xml_output .= "CommentDate=\"";
+	for($y = 0 ; $y < mysql_num_rows($comments_result) ; $y++){
+	
+		$comments_data_row = mysql_fetch_assoc($comments_result);
+		$xml_output .= $comments_data_row['comment_date'];
+	}
+	$xml_output .= '"'."\n";
+	
+	$xml_output .= ">\n";
+	$xml_output .= "</PostArtifcatData>\n";
+	
+}
+
+$xml_output .= "</PostArtificats>\n";
+
+echo $xml_output;
+
+?>
---
+++ readme.txt
@@ -0,0 +1,28 @@
+=== Hanu-Droid ===
+Contributors: mastram
+Tags: Hanu-Droid, Android, Native Mobile App
+Requires at least: 3.0
+Tested up to: 3.4
+
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Hanu-Droid framework allows you to convert your word press blog in to a native Android app.
+
+== Description ==
+Hanu-Droid plugin allows you to convert your word press blog into an Android Native app for free. 
+After you install the plugin, visit: http://hanu-droid.varunverma.org for more details and to get your free Android app. 
+
+With this plugin, you can send push notifications to your app users whenever you add / update any post. To send push notifications, go to the settings - Hanu-Droid Settings and Send notifications.
+
+== Installation ==
+
+The installation is very easy. Just follow the below steps:
+
+1. Upload Hanu-Droid folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Changelog ==
+
+= 1.0 =
+This is the first version.
